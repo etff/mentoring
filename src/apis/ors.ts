a8fb1ca69044cdab6e11452d5504314bc651d3f3
pf-dennis <bh.kim@pet-friends.co.kr>
2024-06-21 01:24:23 +0900

    feat: insert 문 추가

---
+++ src/apis/ors.ts
@@ -0,0 +1,36 @@
+"use client";
+
+import {createSupabaseBrowserClient} from "@/lib/client/supabase";
+
+// todoList 가져오기 + byId
+export const getTodoById = async (id: number) => {
+    const supabase = createSupabaseBrowserClient();
+    const result = await supabase.from("ors")
+        .select("*")
+        .is("deleted_at", null)
+        .eq("id", id);
+
+    return result.data;
+}
+
+
+// todoList 추가
+export const createTodo = async (
+    individual: number,
+    interpersonal: number,
+    social: number,
+    overall: number
+) => {
+    const supabase = createSupabaseBrowserClient();
+    const result = await supabase.from("ors")
+        .insert({
+            individual,
+            interpersonal,
+            social,
+            overall
+        })
+        .select();
+    return result.data;
+}
+
+

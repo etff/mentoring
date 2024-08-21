8abc7597a864f4135928aee56f41c3ffd12a452e
etff <wish2rich@gmail.com>
2024-08-22 08:34:07 +0900

    feat: Feedback 추가

--- src/components/ui/mentee/Main.tsx
+++ src/components/ui/mentee/Main.tsx
@@ -16,28 +16,42 @@ const Main = ({user}: Props) => {
                             ORS / SRS
                         </h1>
                     </header>
-                    <section className="mt-10 text-xl text-center">
+                    <section className="mt-10 text-xl">
+                        <div>
                         <h2>ORS</h2>
                         <p>오늘을 포함해 지난 한주간을 돌아봅시다.</p>
                         <Link
                             href={{
                                 pathname: "/mentee/ors"
                             }}
-                            className="border-2 border-black bg-blue-400 rounded"
+                            className="border-2 border-black bg-blue-400 rounded flex justify-center items-center w-full h-12"
                         >
                             ORS 시작하기
                         </Link>
+                        </div>
                     </section>
-                    <section className="mt-10 text-xl text-center">
+                    <section className="mt-10 text-xl">
                         <h2>SRS</h2>
-                        <p>오늘을 포함해 지난 한주간을 돌아봅시다.</p>
+                        <p>멘토링 종료 후 오늘 세션을 평가해주세요.</p>
                         <Link
                             href={{
                                 pathname: "/mentee/srs"
                             }}
-                            className="border-2 border-black bg-blue-400 rounded"
+                            className="border-2 border-black bg-blue-400 rounded flex justify-center items-center w-full h-12"
                         >
                             SRS 시작하기
+                        </Link>
+                    </section>
+                    <section className="mt-10 text-xl">
+                        <h2>결과보기</h2>
+                        <p>멘토링 평가 내용을 확인할 수 있습니다.</p>
+                        <Link
+                            href={{
+                                pathname: "/mentee/feedback"
+                            }}
+                            className="border-2 border-black bg-blue-400 rounded flex justify-center items-center w-full h-12"
+                        >
+                            결과보기
                         </Link>
                     </section>
                 </article>
